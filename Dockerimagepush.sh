a77ff6df807c207670057d07cf4a2e68f8bbe4ca
Boitai <165635802+Boitai@users.noreply.github.com>
2024-04-03 13:34:42 +0530

    Update Dockerimagepush.sh

--- Dockerimagepush.sh
+++ Dockerimagepush.sh
@@ -4,8 +4,7 @@
 git clone https://github.com/Boitai/Docker_build.git
 
 #Login to Docker Hub
-docker login -u boitai -p Airbenderseries@1234
-
+docker login -u boitai -p password
 # Biuld Docker Image
 docker buildx build --platform linux/amd64,linux/arm64 -t boitai/buildadttest_1:latest
 
